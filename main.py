4b11603b1d7d33f3101e4b64d552175bc1c94335
agat <abdulghaniasykurthoriq@gmail.com>
2022-04-14 20:29:02 +0700

    menambahkan bullets baru

---
+++ main.py
@@ -0,0 +1,36 @@
+import pygame
+from oop.bullets.shot import Shot
+from oop.bullets.fire import Fire
+from oop.control import GameController
+from oop.scene import Scene
+from oop.levels.level1 import Level1
+from oop.levels.level2 import Level2
+from oop.levels.level3 import Level3
+from oop.levels.level4 import Level4
+from oop.levels.level5 import Level5
+from oop.player import Player
+
+pygame.init()
+
+scene = Scene((640, 480))
+scene.setup_level(
+  Level1,
+  Level2,
+  Level3,
+  Level4,
+  Level5,
+)
+
+scene.next_level()
+scene.next_level()
+
+player = Player(scene, Shot())
+game_controller = GameController(player)
+
+running = True
+
+while(running):
+  scene.fill()
+  game_controller.keyboard_event()
+  game_controller.mouse_position_event()
+  pygame.display.flip()
